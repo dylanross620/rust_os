3a8bf3dd37e814e8bfc934aa7ec13dc05f861cf8
Dylan Ross <dylanross620@gmail.com>
2022-03-14 11:38:11 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use os::println;
+
+// Entry point for the program due to its exported name
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    os::init();
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+// Function that will get called in case of a panic. It never returns (as marked by returning `!`)
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+// Special panic handler for testing to use serial output
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
