93b90635db892a4a8cb4a7ce3691e59b655e18a0
Dylan Ross <dylanross620@gmail.com>
2022-03-21 11:57:08 -0400

    Paging

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,15 @@ pub extern "C" fn _start() -> ! {
 
     os::init();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table);
+
     #[cfg(test)]
     test_main();
 
+    println!("It didn't crash");
     os::hlt_loop();
 }
 
